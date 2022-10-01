197da0b944a7407d348c231e69fab8940459ba5d
GrimReapper86 <96471138+GrimReapper86@users.noreply.github.com>
2022-10-01 14:00:38 +0300

    Initial commit

---
+++ player.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    """ Draw a Frog that is being controlled by up and down arrow """
+
+    def __init__(self):
+        super().__init__()
+        self.color('green')
+        self.shape('turtle')
+        self.penup()
+        self.setheading(90)
+        self.speed("fastest")
+        self.go_to_start()
+
+    def up(self):
+        new_cord = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_cord)
+
+    def down(self):
+        new_cord = self.ycor() - MOVE_DISTANCE
+        self.goto(self.xcor(), new_cord)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
